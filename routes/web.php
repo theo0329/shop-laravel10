7c2babf7564e470b0c704fcef0c65046a2df0652
Theo_huang <theo_huang@staff.ruten.com.tw>
2023-12-18 12:18:19 +0800

    新分支

--- routes/web.php
+++ routes/web.php
@@ -37,5 +37,3 @@ Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/addProduct/{productId}', [CartController::class, 'addItem']);
 Route::get('/removeItem/{productId}', [CartController::class, 'removeItem']);
 Route::get('/cart', [CartController::class, 'showCart']);
-
-Route::get('/carts', [CartController::class, 'showCart']);
